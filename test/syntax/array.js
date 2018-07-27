da2fc33274c94d5fc7bf1f09f29fc93ee9e6bfd0
ChanJong <allsser@naver.com>
2018-07-28 06:04:00 +0900

    test1

---
+++ test/syntax/array.js
@@ -0,0 +1,10 @@
+//CRUD Create, Read, Used, Delete
+var arr = ['A', 'Bds', 'C', 12, true];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
+console.log('ar');
---
+++ test/syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6)); //round 함수는 반올림을 해주는 함수이다.
+console.log(Math.round(1.4));
+
+function sum(first, second){ //parameter 파라메터
+  console.log('a');
+  return first + second; //return이 나오면 여기서 끝이난다.
+  console.log('b');
+}
+
+console.log(sum(2,4)); //argument 인자
